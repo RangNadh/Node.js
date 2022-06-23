266284a62d5bcf02d81055b6e66549184b279434
rangolla <ranganadh1509@gmail.com>
2022-06-23 13:20:07 +0530

    commit

--- node/stream/app.js
+++ node/stream/app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + './readMe.txt');
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
 
 myReadStream.on('data', function(chunk){
     console.log('new chunk received:');
