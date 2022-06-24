ff85f2539e10d89f03a60962ed11a444d5c1d465
rangolla <ranganadh1509@gmail.com>
2022-06-24 13:02:44 +0530

    commit

--- Exp/app.js
+++ Exp/app.js
@@ -10,4 +10,7 @@ app.get('/contact', function(req,res){
     res.send('This is the contactpage.')
 });
 
+app.get('/profile/:id', function(req,res){
+    res.send('you requested to see a profile with the id of ' + req.params.id);
+});
 app.listen(3000);
