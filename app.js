e3cae344369c4fdd284f5f7e11c63b84509cb69f
rangolla <ranganadh1509@gmail.com>
2022-06-24 15:31:26 +0530

    commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req,res){
     res.send('This is the homepage.');
     //res.sendFile(__dirname + '/index.html');
@@ -11,7 +13,8 @@ app.get('/contact', function(req,res){
     res.send('This is the contactpage.');
 });
 
-app.get('/profile/:id', function(req,res){
-    res.send('you requested to see a profile with the id of ' + req.params.id);
+app.get('/profile/:name', function(req,res){
+    var data = {age: 29, job: 'software'};
+    res.render('profile', {person: req.params.name, data: data});
 });
 app.listen(3000);
