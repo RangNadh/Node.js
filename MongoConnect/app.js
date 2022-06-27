97da11e26378d2ea4dae7df99dc4e543f470ba8a
rangolla <ranganadh1509@gmail.com>
2022-06-27 15:50:41 +0530

    commit

---
+++ MongoConnect/app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Book = require('./book_model');
+
+var db = 'mongodb+srv://ranganadh:ran@cluster0.dyzt1.mongodb.net/mongoose';
+mongoose.connect(db);
+
+app.get('/', function(req, res){
+    res.send('happy to be here.');
+});
+
+app.get('/books', function(req, res){
+    console.log('getting all books');
+    Book.find({})
+    .exec(function(err, books){
+        if(err){
+            res.send('error has occured');
+        }else {
+            res.json(books);
+        }
+    });
+});
+
+app.get('/books/:id', function(req, res){
+    console.log('getting one book');
+    Book.findOne({
+        _id:req.params.id
+    });
+    exec(function(err,book){
+        if(err){
+            res.send('error occured');
+        }else{
+            console.log(book);
+            res.json(book);
+        }
+    });
+});
+
+var port = 8080;
+
+app.listen(port, function(){
+    console.log('app listening on port ' + port);
+});
--- MongoConnect/book_model.js
+++ MongoConnect/book_model.js
@@ -3,22 +3,7 @@ var Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
     title: String,
-    published:{
-        type: Date,
-        default: Date.now
-    },
-    keywords: Array,
-    published: Boolean,
-    author:{
-        type: Schema.Objectid,
-        ref: 'User'
-    },
-    // Embedded sub-document
-    detail: {
-        modelNumber: Number,
-        hardcover: Boolean,
-        reviews: Number,
-        rank: Number
-    }
+    author: String,
+    category: String
 });
 module.exports = mongoose.model('Book', BookSchema);
